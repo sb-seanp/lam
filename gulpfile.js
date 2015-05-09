5b41556c67e8352e74bd878b80b36ac36eb0682d
Sean Prasertsit <sean@prasertsit.com>
2015-05-08 19:23:22 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp        = require('gulp'),
+    server      = require('gulp-express'),
+    concat      = require('gulp-concat'),
+    jade        = require('gulp-jade'),
+    stylus      = require('gulp-stylus'),
+    nib         = require('nib'),
+    jshint      = require('gulp-jshint'),
+    stylish     = require('jshint-stylish'),
+    path        = require('path');
+
+// Lint javascript
+gulp.task('lint', function(){
+    gulp.src('public/javascripts/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+// Compile Stylus stylesheets
+gulp.task('stylus', function(){
+    gulp.src('public/stylesheets/*.styl')
+        .pipe(stylus({
+            use: [nib()],
+            compress: true
+        }))
+        .pipe(gulp.dest('public/stylesheets/'))
+});
+
+// Compile Jade templates
+gulp.task('jade', function(){
+    gulp.src('views/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('./views/'))
+});
+
+// Watch for changes and restart server if necessary
+gulp.task('server', function(){
+    // Start server
+    server.run({
+        file: 'bin/wulfcode'
+    });
+    gulp.watch('routes/*.js', ['server']);
+    gulp.watch('public/javascripts/*.js', ['lint']);
+    gulp.watch('public/stylesheets/*.styl', ['stylus']);
+    gulp.watch('views/*.jade', ['jade']);
+});
+
+gulp.task('deploy', ['lint', 'stylus', 'jade']);
+
+// Default task
+gulp.task('default', ['lint', 'stylus', 'jade', 'server']);
